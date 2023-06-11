d283b545cb98661103e837099d621baf73f675b1
bluehead01 <main@bluehead.dev>
2023-06-11 14:10:28 +0900

    config: prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,20 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
     // Recommended ts/es-lint rules
     'plugin:@typescript-eslint/recommended',
-    // Disable ESLint rules conflicting with Prettier
+    // Disable ESLint rules conflicting with Prettier\
     'prettier',
-    // Displays Prettier  errors as ESLint errors - must be last plugin in 'extends' array
-    'plugin:prettier/recommended',
   ],
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     ecmaVersion: 6,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
       modules: true,
     },
   },
-  ignorePatterns: ['.eslintrc.js']
-}
+  ignorePatterns: ['.eslintrc.js'],
+};
